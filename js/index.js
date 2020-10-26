21d20a59b12524a179ad0d8a6776e5164c74068b
brennanlawrence <brennanlawrence2002@gmail.com>
2020-10-26 15:17:03 -0600

    Solved MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,84 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const link = document.querySelectorAll("nav a");
+link[0].innerText = "Services"; 
+link[0].style.color = "green";
+link[1].innerText = "Product";
+link[1].style.color = "green";
+link[2].innerText = "Vision";
+link[2].style.color = "green";
+link[3].innerText = "Features";
+link[3].style.color = "green";
+link[4].innerText = "About";
+link[4].style.color = "green";
+link[5].innerText = "Contact";
+link[5].style.color = "green";
+
+const fAQ = document.createElement("A");
+fAQ.innerText = "FAQs";
+fAQ.style.color = "green";
+document.querySelector("nav").appendChild(fAQ);
+
+const blog = document.createElement("A");
+blog.innerText = "Blog";
+blog.style.color = "green";
+document.querySelector("nav").prepend(blog);
+
+//const styleLink = document.querySelectorAll("nav a");
+//styleLink.style.color = "green";
+
+let h1Text = document.querySelector("h1");
+h1Text.innerText = `DOM\nIS\nAWESOME`;
+
+let bigButton = document.querySelector("button");
+bigButton.innerText = "Get Started";
+
+let headerImg = document.querySelector(".cta img");
+headerImg.setAttribute("src", "../img/header-img.png")
+
+const h4sTop = document.querySelectorAll(".top-content h4")
+h4sTop[0].innerText = "Features";
+h4sTop[1].innerText = "About";
+
+const psTop = document.querySelectorAll(".top-content p")
+psTop[0].innerText = `Features content elementum magna eros, ac posuere elvit 
+tempus et. Suspendisse vel tempus odio,in interdutm nisi.
+Suspendisse eu ornare nisl. Nullam convallis augue justo, at
+imperdiet metus scelerisque quis.`;
+psTop[1].innerText = `About content elementum magna eros, ac posuere elvit 
+tempus et. Suspendisse vel tempus odio,in interdutm nisi.
+Suspendisse eu ornare nisl. Nullam convallis augue justo, at
+imperdiet metus scelerisque quis.`;
+
+let middleImg = document.querySelector(".middle-img");
+middleImg.setAttribute("src", "../img/mid-page-accent.jpg");
+
+const h4sBottom = document.querySelectorAll(".bottom-content h4")
+h4sBottom[0].innerText = "Services";
+h4sBottom[1].innerText = "Product";
+h4sBottom[2].innerText = "Vision";
+
+const psBottom = document.querySelectorAll(".bottom-content p")
+psBottom[0].innerText = `Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+psBottom[1].innerText = `Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+psBottom[2].innerText = `Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+
+
+const contactTitle = document.querySelector(".contact h4");
+contactTitle.innerText = "Contact";
+const contactInfo = document.querySelectorAll(".contact p");
+contactInfo[0].innerText = `123 Way 456 Street
+Somewhere, USA`;
+contactInfo[1].innerText = "1 (888) 888-8888";
+contactInfo[2].innerText = "sales@greatidea.io";
+
+const footerP = document.querySelector("footer p");
+footerP.innerText = "Copyright Great Idea! 2018";
